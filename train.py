7c0dde56fd11316cad5964101cbbbdd81bcbf5d3
Vijay Gaikwad <vijaygkd@gmail.com>
2023-04-04 16:57:17 +0530

    better tqdm printing

--- train.py
+++ train.py
@@ -20,11 +20,11 @@ def train():
     p_drop = 0.1
 
     # TRAINING PARAMETERS #
-    num_epochs = 10
+    num_epochs = 50
     batch_size = 32
     seq_len = 50
     lr=0.001    # default=0.001
-    model_path = 'model/tinygpt.pt'
+    model_path = 'model/tinygpt_ws.pt'
     data_path = 'data/tinyshakespeare.txt'
 
     # ---------------------------------------- #
@@ -37,8 +37,9 @@ def train():
     dataset = GPTDataset(data_path, seq_len=seq_len)
     vocab_size = dataset.tokenizer.vocab_size
     dataloader = DataLoader(dataset, batch_size, shuffle=True, collate_fn=pad_seq_fn)
+    num_batches = len(dataloader)
     print(f"Dataset token size: {len(dataset)}")
-    print(f"No. of batches: {len(dataloader)}")
+    print(f"No. of batches: {num_batches}")
 
     # model
     gpt = GPT(
@@ -68,32 +69,39 @@ def train():
         gpt.train()
 
         running_loss = 0.0
-        for i, data in enumerate(tqdm(dataloader, desc=f'Epoch {epoch}', unit='batch')):
-            X, Y = data
-            X, Y = X.to(device), Y.to(device)
-            # Zero the gradients
-            optimizer.zero_grad()
+        with tqdm(total=num_batches, desc=f'Epoch {epoch}', unit='batch') as pbar:
+            # for i, data in enumerate(tqdm(dataloader, desc=f'Epoch {epoch}', unit='batch')):
+            for i, data in enumerate(dataloader):
+                X, Y = data
+                X, Y = X.to(device), Y.to(device)
+                # Zero the gradients
+                optimizer.zero_grad()
 
-            # Forward pass
-            logits, attns = gpt(X)
-            #y_pred = torch.argmax(logits, dim=-1)
-            logits = logits.view(-1, vocab_size)
-            targets = Y.view(-1)
-            
-            # Compute the loss
-            loss = criterion(logits, targets)
+                # Forward pass
+                logits, attns = gpt(X)
+                #y_pred = torch.argmax(logits, dim=-1)
+                logits = logits.view(-1, vocab_size)
+                targets = Y.view(-1)
+                
+                # Compute the loss
+                loss = criterion(logits, targets)
 
-            # Backward pass
-            loss.backward()
+                # Backward pass
+                loss.backward()
 
-            # Update the weights
-            optimizer.step()
+                # Update the weights
+                optimizer.step()
 
-            # print statistics
-            running_loss += loss.item()
-            if i % 10 == 9:    # print every 10 mini-batches
-                # print(f'Epoch {epoch}, batch {i+1}: loss = {running_loss / 10:.4f}')
-                running_loss = 0.0
+                # print statistics
+                pbar.set_postfix(loss=loss.item())
+                pbar.update()
+
+                running_loss += loss.item()
+                if i % 10 == 9:    # print every 10 mini-batches
+                    # print(f'Epoch {epoch}, batch {i+1}: loss = {running_loss / 10:.4f}')
+                    # Update the tqdm progress bar with the current loss value
+                    # pbar.set_postfix(loss=f'{running_loss / 10:.4f}')
+                    running_loss = 0.0
 
         # Print the loss for this epoch
         print("Epoch [{}/{}], Loss: {:.4f}".format(epoch+1, num_epochs, loss.item()))
