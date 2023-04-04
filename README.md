7dbbc4daba2f3d679bf80aeb6cdebbcec849e16b
Vijay Gaikwad <vijaygkd@gmail.com>
2023-04-04 18:59:26 +0530

    implement nucleus sampling

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ GPT-2 implementation using PyTorch
 
 # TODO
 - Pad token masking
-- Inference - generating tokens
+- <s>Inference - generating tokens</s>
 - Update unit tests
 - how to process text data? <bos>,<eos>,<pad>. How to split text file?
 - Better data handling? Train on larger dataset.
-- Reuse token embedding layer in the output layer
-- Track training loss
+- <s>Reuse token embedding layer in the output layer</s>
+- Track training loss - WnB
+
 
 
 # Architecture
@@ -35,6 +36,9 @@ A. Transformer architecture
     - padding mask
     - subsequent mask
 
+## Model Size
+![gtp2 model size](https://jalammar.github.io/images/gpt2/gpt2-sizes-hyperparameters-3.png)
+GPT-2 model sizes! No. of decoder stacks vs no. of parameters
 
 # Refs
 https://rowlando13.medium.com/everything-gpt-2-4-data-preparation-514cb62f9f3b
--- predict.py
+++ predict.py
@@ -5,20 +5,25 @@ import torch
 from transformers import GPT2TokenizerFast
 from GPT import GPT
 
-# TODO - slidding window text generation
-# TODO impelemnt nucleus decoding - top_p
-def generate_text(model, context=" ", max_len=100, device='cpu'):
+
+def generate_text(model, context=" ", top_p=0.9, temperature=1, max_len=100, device='cpu'):
     tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    seq_len = model.seq_len
 
     show(context, color=False)
     # inference
     model.eval()
     for i in range(max_len):
         # (seq_len)
+        # truncate context to max seq_len
+        tokens = tokenizer.tokenize(context)
+        tokens = tokens[-seq_len:]              
+        context = tokenizer.convert_tokens_to_string(tokens)
+        # encode text
         context_tokens = tokenizer(context, 
                                     padding='max_length',
-                                    max_length=model.seq_len,
+                                    max_length=seq_len,
                                     return_tensors='pt'
                                     ).to(device)
         input = context_tokens['input_ids']
@@ -27,20 +32,16 @@ def generate_text(model, context=" ", max_len=100, device='cpu'):
         logits, attn = model(input)                             # logit: (1, seq_len, vocab)
         next_token_idx = attn_mask.to(torch.int32).sum() - 1  
         logits = logits.squeeze()                               # (seq_len, vocab)
-        token_logits = logits[next_token_idx]                # (vocab)
+        token_logits = logits[next_token_idx]                   # (vocab)
         # sample token
-        next_token = nucleus_sampling(token_logits, top_p=0.9, temperature=0.3)
+        next_token = nucleus_sampling(token_logits, top_p, temperature)
         pred_word = tokenizer.decode(next_token)
         show(pred_word, color=True)
         # auto-regressive generation
         context += pred_word
 
-        # TODO - allow moving window
-        if next_token_idx == model.seq_len-1:
-            break
 
-
-def nucleus_sampling(logits, top_p=0.9, temperature=1):
+def nucleus_sampling(logits, top_p, temperature):
     # logits: (vocab)
     temp_logits = logits / temperature
     probs = torch.softmax(temp_logits, dim=-1)  # (vocab)
@@ -48,25 +49,24 @@ def nucleus_sampling(logits, top_p=0.9, temperature=1):
     return token
 
 
-def top_p_sampling(probabilities, top_p=0.9):
+def top_p_sampling(probabilities, top_p):
     # Sort the probabilities in descending order
     sorted_probs, sorted_indices = torch.sort(probabilities, descending=True)
     # Compute the cumulative probabilities
     cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
     # Find the indices to keep
-    keep_indices = (cumulative_probs <= top_p).nonzero()
+    top_p = max(top_p, cumulative_probs[0].item())
+    keep_indices = (cumulative_probs <= top_p).nonzero().view(-1)
     # Select the top-p probabilities and indices
-    top_p_probs = sorted_probs[keep_indices].squeeze()
-    top_p_indices = sorted_indices[keep_indices].squeeze()
-    # Create a mask to zero-out all other probabilities
-    mask = torch.zeros_like(probabilities).scatter(1, top_p_indices.unsqueeze(1), 1.0)
-    # Apply the mask to the probabilities
-    masked_probs = mask * probabilities
+    top_p_probs = sorted_probs[keep_indices].view(-1)
+    top_p_indices = sorted_indices[keep_indices].view(-1)
     # Normalize the probabilities
-    normalized_probs = masked_probs / masked_probs.sum(dim=-1, keepdim=True)
+    normalized_probs = top_p_probs / top_p_probs.sum(dim=-1, keepdim=True).view(-1)
     # Sample from the normalized probabilities
-    samples = torch.multinomial(normalized_probs, num_samples=1)
-    return samples.squeeze()
+    p_sample_idx = torch.multinomial(normalized_probs, num_samples=1, replacement=True)
+    # get corresponding token id
+    token = top_p_indices[p_sample_idx]
+    return token
 
 
 
@@ -85,6 +85,10 @@ def show(text, color=False):
     
 
 if __name__ == '__main__':
-    context = """God is"""
-    gpt = torch.load('model/tinygpt.pt')
-    generate_text(gpt, context, device='mps')
+    context = """Man is a slave of God. Thee should"""
+    gpt = torch.load('model/tinygpt_ws.pt')
+    generate_text(gpt, context, 
+                  top_p=0.95,
+                  temperature=1, 
+                  max_len=100,
+                  device='mps')
