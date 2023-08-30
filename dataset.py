5774de237f92657732212a191c26ee2e32afca9e
Vijay Gaikwad <vijaygkd@gmail.com>
2023-08-30 15:16:41 -0700

    char tokenizer

--- dataset.py
+++ dataset.py
@@ -10,17 +10,15 @@ from torch.utils.data import Dataset, DataLoader
 from transformers import GPT2TokenizerFast
 
 
+
 class GPTDataset(Dataset):
     """
     Read a text file and break in into chunks of size seq_len.
-    Returns text encoding using byte-pair encoding as input and label pairs.
-    Label is the right shifted input by 1 position to next-token prediction task.
-    Eg.Text: "I love tennis!" Input: ['I', 'love', 'tennis'] and label: ['love', 'tennis', '!']
     """
-    def __init__(self, file_path, seq_len):
+    def __init__(self, tokenizer, file_path, seq_len):
         self.seq_len = seq_len   
         split_size = seq_len + 1       # right shit input by 1 position to create labels for training
-        self.tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
+        self.tokenizer = tokenizer 
         
         with open(file_path, 'r', encoding='utf-8') as f:
             self.text = f.read()
@@ -34,25 +32,72 @@ class GPTDataset(Dataset):
 
     def __getitem__(self, idx):
         # Convert the example to input and label tensors. 
-        example = torch.tensor(self.examples[idx], dtype=torch.long)
+        example = self.examples[idx]
+        example = torch.tensor(example, dtype=torch.long)
         input = example[:-1]
         # Label right shifted by 1.
         label = example[1:]
         return (input, label)
 
 
-def pad_seq_fn(batch):
-    # Pad the examples to the same length
+def pad_seq_fn(batch, pad_id):
+    # Pad the examples to the same length # GPT2 tokenizer <eos> as 50256
     inputs = [item[0] for item in batch]
     labels = [item[1] for item in batch]
-    inputs = pad_sequence(inputs, batch_first=True, padding_value=50256)    # GPT2 tokenizer <eos> as 50256
-    labels = pad_sequence(labels, batch_first=True, padding_value=50256)
+    inputs = pad_sequence(inputs, batch_first=True, padding_value=pad_id)    
+    labels = pad_sequence(labels, batch_first=True, padding_value=pad_id)
     return inputs, labels
 
 
+#GPT2TokenizerFast.from_pretrained('gpt2')
+# Returns text encoding using byte-pair encoding as input and label pairs.
+# Label is the right shifted input by 1 position to next-token prediction task.
+# Eg.Text: "I love tennis!" Input: ['I', 'love', 'tennis'] and label: ['love', 'tennis', '!']
+
+
+########################
+# Char level tokenizer and dataset
+########################
+class CharTokenizer():
+    """
+    Tokenizer is character level ASCII (0-127). Use eos_token at start and in between samples.
+    Label is right shifted input by 1 position to next-token prediction task.
+    Eg.Text: "I love tennis!" Input: [<eos>, 'I', ' ', 'l', 'o', 'v', 'e', ' ', 't', 'e', 'n', 'n', 'i', 's', '!', <eos>]
+    """
+    def __init__(self):
+        self.eos_token_id = 127
+        self.eos_token = chr(self.eos_token_id)
+
+    def tokenize(self, text):
+        tokens = [self.eos_token] + list(text) + [self.eos_token]
+        return tokens
+
+    def convert_tokens_to_ids(self, tokens):
+        return [ord(token) for token in tokens]
+    
+    def convert_ids_to_tokens(self, ids):
+        return [chr(id) for id in ids]
+    
+    def encode(self, text):
+        tokens = self.tokenize(text)
+        ids = self.convert_tokens_to_ids(tokens)
+        return ids
+    
+    def decode(self, ids):
+        tokens = self.convert_ids_to_tokens(ids)
+        text = ''.join(tokens)
+        return text
+    
+
+
 if __name__ == '__main__':
-    dataset = GPTDataset('tests/data.txt', seq_len=10)
-    dataloader = DataLoader(dataset, batch_size=2, collate_fn=pad_seq_fn)
+    tkz = GPT2TokenizerFast.from_pretrained('gpt2')
+    chr_tkz = CharTokenizer()
+
+    dataset = GPTDataset(tkz, 'tests/data.txt', seq_len=10)
+    dataloader = DataLoader(dataset, batch_size=3, collate_fn=lambda x: pad_seq_fn(x, chr_tkz.eos_token_id))
+
     for X, y in dataloader:
         print(X)
         print(y)
+    
